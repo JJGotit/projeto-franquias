04e0d8a69e15dfad54a871e53700bcf38651577d
JJGotit <gadjongo@hotmail.com>
2025-11-06 02:09:25 -0300

    feat: backend api funcionarios

---
+++ src/app/api/funcionarios/route.js
@@ -0,0 +1,99 @@
+//Importar o nextResponse da lib next/server.
+import { NextResponse } from 'next/server'
+//Importar o client do prisma, pois ele que permite eu utilizar os comandos/codigos
+import { PrismaClient } from '../../../generated/prisma/client'
+
+//Criar uma instancia/objeto/item do prisma
+const prisma = new PrismaClient()
+
+export async function GET() {
+    try {
+        const funcionarios = await prisma.funcionario.findMany({
+            include: {
+                franquia: true // Aqui eu incluo a franquia do funcionario na resposta
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+
+        // Verificao se o array/objeto/resultado do banco esta vindo vazio, e se tiver eu passo uma mensagem apropriada
+        if (funcionarios.length === 0) {
+            return NextResponse.json(
+                { error: 'Nenhum funcionário encontrado' },
+                { status: 404 }
+            )
+        }
+
+        return NextResponse.json(funcionarios);
+
+    } catch (error) {
+        console.error('Erro ao buscar funcionários', error);
+        return NextResponse.json(
+            { error: 'Error interno de servidor!' },
+            { status: 500 }
+        )
+    }
+}
+
+export async function POST(request) {
+    try {
+        const data = await request.json()
+        const { nome, email, cargo, salario, franquiaId } = data;
+
+        //Validacao de todos os dados sao obrigatorios
+
+        if (!nome || !email || !cargo || !salario || !franquiaId) {
+            return NextResponse.json(
+                { error: 'Todos os campos são obrigatórios!' },
+                { status: 400 }
+            )
+        }
+
+        // Verificar se existe a franquia que eu quero cadastrar meu funcionario
+        const franquiaExiste = await prisma.franquia.findUnique({
+            where: { id: parseInt(franquiaId) }
+        })
+
+        if (!franquiaExiste) {
+            return NextResponse.json(
+                { error: 'Id da franquia não existe, verifique.' },
+                { status: 404 }
+            )
+        }
+
+        // Verificar se o email é unico
+
+        const emailExiste = await prisma.funcionario.findUnique({
+            where: { email }
+        })
+
+        if (emailExiste) {
+            return NextResponse.json(
+                { error: 'Email já está em uso, tente outro!' },
+                { status: 400 }
+            )
+        }
+
+        // Criar o funcionario
+        const funcionario = await prisma.funcionario.create({
+            data: {
+                nome,
+                email,
+                cargo,
+                salario: parseFloat(salario),
+                franquiaId: parseInt(franquiaId)
+            }
+        })
+
+        // Retornar a resposta
+        return NextResponse.json(funcionario, { status: 201 })
+
+    } catch (error) {
+        console.error('Erro ao criar funcionário', error);
+        return NextResponse.json(
+            { error: 'Error interno de servidor!' },
+            { status: 500 }
+        )
+    }
+}
